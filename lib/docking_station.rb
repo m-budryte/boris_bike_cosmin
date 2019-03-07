941f6cf232da72587647644eb50cc03d2eb63ace
masha-b <mashabudryte@gmail.com>
2019-03-07 16:56:41 +0000

    Removing 'magic numbers'

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,7 @@
 require './lib/bike.rb'
 class DockingStation
   attr_reader :bikes
+  DEFAULT_CAPACITY = 20
   def initialize
     @bikes = []
   end
@@ -18,7 +19,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.count >= 20
+    @bikes.count >= DEFAULT_CAPACITY
   end
 
   def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -25,7 +25,7 @@ describe DockingStation do
     # end
 
     it 'only docks no more than 20 bikes' do
-      20.times { subject.dock(Bike.new) }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
       expect {subject.dock(Bike.new)}.to raise_error 'Capacity is reached'
     end
 end
